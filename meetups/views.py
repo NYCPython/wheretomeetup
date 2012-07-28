b37a8c710dae21aeb3bc801116dfab463e498b79
Andy Dirnberger <dirn@dirnonline.com>
2012-07-28 11:24:00 -0400

    Add placeholders for group and space sign up pages
    Unfortunately I upgraded to Mountain Lion and didn't think to install the new Xcode at the time. Now I can't use Pip yet (to begin using WTF). These are placeholder views and templates to get ready for building them out.

--- meetups/views.py
+++ meetups/views.py
@@ -5,3 +5,13 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/have/')
+def have():
+    return render_template('have.html')
+
+
+@app.route('/need/')
+def need():
+    return render_template('need.html')
