62591fc79f422ea55041fc892fb2082151cf29ef
Dan Crosta <dcrosta@late.am>
2012-08-18 08:59:51 -0400

    complete oauth login/logout flow

--- README.rst
+++ README.rst
@@ -3,3 +3,16 @@ Meetups
 #######
 
 A site for connecting groups and locations. More information coming soon.
+
+
+===========
+Setup Notes
+===========
+
+You will need to create an application at Meetup.com at
+http://www.meetup.com/meetup_api/oauth_consumers/create/. Create a file at
+the root of this repository named ``secrets.cfg``, with the contents::
+
+    MEETUP_OAUTH_CONSUMER_KEY = 'your-meetup-application-key'
+    MEETUP_OAUTH_CONSUMER_SECRET = 'your-meetup-application-secret'
+
--- meetups/models.py
+++ meetups/models.py
@@ -1,13 +1,26 @@
-from flask.ext.login import LoginManager, UserMixin
+from flask import session
+
+from flask.ext.login import LoginManager, UserMixin, AnonymousUser
 
 login_manager = LoginManager()
 
 
 @login_manager.user_loader
 def load_user(id):
-    return User.get(id)
+    member_id = session.get('meetup_member_id')
+    if member_id == id:
+        return User(session['meetup_member_id'], session['user_name'])
+    return None
 
 
 class User(UserMixin):
     class Meta:
         collection = 'users'
+
+    def __init__(self, meetup_member_id, user_name):
+        self.id = self.meetup_member_id = meetup_member_id
+        self.user_name = user_name
+
+class Guest(AnonymousUser):
+    user_name = 'Guest'
+login_manager.anonymous_user = Guest
