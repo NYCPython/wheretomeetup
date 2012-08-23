9d1158bc8a76f85c3b3433c2d74ce4ead79f1b50
Dan Crosta <dcrosta@late.am>
2012-08-22 20:27:21 -0400

    Heroku setup & config tweaks using Flask-Heroku
    Note that we need to use PyMongo 2.3 to support MongoDB 2.2 with
    authentication, which MongoHQ have given us. See
    http://api.mongodb.org/python/2.3rc1/changelog.html#changes-in-version-2-3
    for details.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --debug --log-level=debug --error-logfile=- -w 4 -b 0.0.0.0:$PORT runserver:application
--- meetups/__init__.py
+++ meetups/__init__.py
@@ -1,17 +1,34 @@
+import os
+
 import sendgrid
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.oauth import OAuth
 from flask.ext.pymongo import PyMongo
+from flask.ext.heroku import Heroku
 
 app = Flask(__name__)
-app.secret_key = 'Bn1dcC2QDWXgtj'
+
+heroku = Heroku(app)
 
 try:
     app.config.from_pyfile('../secrets.cfg')
 except IOError:
     # app.log.warn('could not load secrets.cfg')
     pass
+
+def conf(key, default=''):
+    """Helper to load a configuration from `app.config`, if it
+    exists, or from `os.environ` (and saving into `app.config`).
+    """
+    if key in app.config:
+        return app.config[key]
+    elif key in os.environ:
+        app.config[key] = os.environ[key]
+        return app.config[key]
+    return default
+
+app.secret_key = conf('FLASK_SECRET_KEY', 'Bn1dcC2QDWXgtj')
 
 Bootstrap(app)
 
@@ -21,15 +38,22 @@ meetup = oauth.remote_app('meetup',
     request_token_url='https://api.meetup.com/oauth/request/',
     access_token_url='https://api.meetup.com/oauth/access/',
     authorize_url='http://www.meetup.com/authorize/',
-    consumer_key=app.config.get('MEETUP_OAUTH_CONSUMER_KEY', ''),
-    consumer_secret=app.config.get('MEETUP_OAUTH_CONSUMER_SECRET', ''),
+    consumer_key=conf('MEETUP_OAUTH_CONSUMER_KEY'),
+    consumer_secret=conf('MEETUP_OAUTH_CONSUMER_SECRET'),
 )
 
-mongo = PyMongo(app)
+if all(key in app.config for key in ('MONGODB_HOST','MONGODB_HOST','MONGODB_HOST','MONGODB_HOST','MONGODB_HOST')):
+    # this looks like we're on Heroku, so translate
+    # Flask-Heroku's vars to what PyMongo expects
+    app.config['MONGODB_DBNAME'] = app.config.pop('MONGODB_DB')
+    app.config['MONGODB_USERNAME'] = app.config.pop('MONGODB_USER')
+    mongo = PyMongo(app, config_prefix='MONGODB')
+else:
+    mongo = PyMongo(app)
 
 sendgrid_api = sendgrid.Sendgrid(
-    username=app.config.get('SENDGRID_USERNAME', ''),
-    password=app.config.get('SENDGRID_PASSWORD', ''),
+    username=conf('SENDGRID_USERNAME'),
+    password=conf('SENDGRID_PASSWORD'),
     secure=True,
 )
 
