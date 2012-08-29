407b1dc953e08ab5555dfd52491f1d37b86c4342
Julian Berman <Julian+git@GrayVines.com>
2012-08-29 01:28:13 -0400

    And some from the model tests.

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,15 +3,17 @@ from unittest import TestCase
 import mock
 
 from meetups import models
+from tests.utils import PatchMixin
 
 
-class TestModel(TestCase):
+class TestModel(TestCase, PatchMixin):
+    def setUp(self):
+        self.load = self.patch("meetups.models.Model.load")
+
     def test_fetch_by_id_gets_the_model(self):
-        with mock.patch("meetups.models.Model.load") as load:
-            m = models.Model.with_id(12)
-            self.assertEqual(m._id, 12)
+        m = models.Model.with_id(12)
+        self.assertEqual(m._id, 12)
 
     def test_fetch_by_id_loads_the_model(self):
-        with mock.patch("meetups.models.Model.load") as load:
-            m = models.Model.with_id(12)
-            load.assert_called_once_with()
+        m = models.Model.with_id(12)
+        self.load.assert_called_once_with()
