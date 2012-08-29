7d5a7ce344a32d49926cb48f14176a61b131d4d4
Julian Berman <Julian+git@GrayVines.com>
2012-08-29 01:25:11 -0400

    Removed some view test duplication.

--- tests/test_views.py
+++ tests/test_views.py
@@ -4,51 +4,39 @@ from flask.ext.testing import TestCase
 import mock
 
 from meetups import app, views
+from tests.utils import PatchMixin
 
 
-class TestLoginSync(TestCase):
+class TestLoginSync(TestCase, PatchMixin):
     """
     The login sync view syncs a user with the Meetup API.
 
     """
+
+    def setUp(self):
+        self.client = self.app.test_client()
+        self.member_id = 1
+
+        with self.client.session_transaction() as session:
+            session["member_id"] = self.member_id
+
+        self.User = self.patch("meetups.views.User")
+        self.login_user = self.patch("meetups.views.login_user")
+        self.sync_user = self.patch("meetups.views.sync_user")
 
     def create_app(self):
         app.config["TESTING"] = True
         return app
 
     def test_syncs_a_loaded_user(self):
-        with self.app.test_client() as client:
-            with client.session_transaction() as session:
-                session["member_id"] = 1
-
-            with mock.patch("meetups.views.User") as User:
-                with mock.patch("meetups.views.sync_user") as sync_user:
-                    with mock.patch("meetups.views.login_user") as login_user:
-                        user = User.with_id(session["member_id"])
-                        client.get("/login/sync/")
-
-            sync_user.assert_called_once_with(user)
+        user = self.User.with_id(self.member_id)
+        self.client.get("/login/sync/")
+        self.sync_user.assert_called_once_with(user)
 
     def test_logs_in_user(self):
-        with self.app.test_client() as client:
-            with client.session_transaction() as session:
-                session["member_id"] = 1
-
-            with mock.patch("meetups.views.User") as User:
-                with mock.patch("meetups.views.sync_user") as sync_user:
-                    with mock.patch("meetups.views.login_user") as login_user:
-                        client.get("/login/sync/")
-
-            self.assertTrue(login_user.called)
+        self.client.get("/login/sync/")
+        self.assertTrue(self.login_user.called)
 
     def test_redirects_to_profile_after_sync(self):
-        with self.app.test_client() as client:
-            with client.session_transaction() as session:
-                session["member_id"] = 1
-
-            with mock.patch("meetups.views.User") as User:
-                with mock.patch("meetups.views.sync_user") as sync_user:
-                    with mock.patch("meetups.views.login_user") as login_user:
-                        response = client.get("/login/sync/")
-
-            self.assertRedirects(response, url_for("user_profile"))
+        response = self.client.get("/login/sync/")
+        self.assertRedirects(response, url_for("user_profile"))
